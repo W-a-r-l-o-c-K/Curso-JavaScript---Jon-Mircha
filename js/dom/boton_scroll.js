c45463947eab5cc539982b8548142d9d38f33b77
W-a-r-l-o-c-K <juanma0010@gmail.com>
2022-10-05 11:36:17 +0200

    Update 8

--- js/dom/boton_scroll.js
+++ js/dom/boton_scroll.js
@@ -1,1 +1,30 @@
 /* **********     Curso JavaScript: 87. DOM: Ejercicios Prácticos | Botón de Scroll - #jonmircha     ********** */
+
+const d = document,
+w = window;
+
+export default function  scrollTopButton(btn) {
+  const $scollBtn = d.querySelector(btn);
+
+  w.addEventListener("scroll", (e) => {
+    let scrollTop = w.pageYOffset || d.documentElement.scrollTop;
+
+    if (scrollTop > 800) {
+      $scollBtn.classList.remove("hidden");
+    } else {
+      $scollBtn.classList.add("hidden");
+    }
+
+    // console.log(w.pageYOffset, d.documentElement.scrollTop);
+  });
+  d.addEventListener("click", (e) => {
+    if (e.target.matches(btn)) {
+      w.scrollTo({
+        behavior: "smooth",
+        top: 0,
+        //left: 0,
+      });
+    }
+
+  });
+}
--- js/index_dom.js
+++ js/index_dom.js
@@ -3,6 +3,9 @@
 import hamburguerMenu from "./dom/menu_hamburguesa.js";
 import { digitalClock,alarm } from "./dom/reloj.js";
 import { moveBall, shorcuts } from "./dom/teclado.js";
+import countdown from "./dom/cuenta_regresiva.js";
+import scrollTopButton from "./dom/boton_scroll.js";
+import darkTheme from "./dom/tema_oscuro.js";
 
 const d = document;
 
@@ -10,6 +13,8 @@ d.addEventListener("DOMContentLoaded", (e) => {
   hamburguerMenu(".panel-btn", ".panel", ".menu a");
   digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
   alarm("assets/alarma.mp3", "#activar-alarma", "#desactivar-alarma");
+  countdown("countdown", "Oct 05, 2022 09:56:19", "Feliz cumpleaños, amigo y docente digital 🤓");
+  scrollTopButton(".scroll-top-btn");
 });
 
 d.addEventListener("keydown", (e) => {
@@ -17,3 +22,4 @@ d.addEventListener("keydown", (e) => {
   moveBall(e, ".ball", ".stage");
 })
 
+darkTheme(".dark-theme-btn", "dark-mode");
