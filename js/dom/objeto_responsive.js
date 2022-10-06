727a12243c538b1f3f272e1ed42d5aeb6677f65e
W-a-r-l-o-c-K <juanma0010@gmail.com>
2022-10-06 15:58:12 +0200

    Update 9

--- js/dom/objeto_responsive.js
+++ js/dom/objeto_responsive.js
@@ -1,1 +1,21 @@
 /* **********     Curso JavaScript: 90. DOM: Ejercicios Prácticos | Responsive con JavaScript - #jonmircha     ********** */
+
+const d = document,
+  w = window;
+
+export default function responsiveMedia(id, mediaquery, mobileContent, desktopContent) {
+  let breakpoint = w.matchMedia(mediaquery);
+
+  const responsive = (e) => {
+    if (e.matches) {
+      d.getElementById(id).innerHTML = desktopContent;
+    } else {
+      d.getElementById(id).innerHTML = mobileContent;
+    }
+
+    //console.log("MediaQuery: ",breakpoint, e.matches);
+  };
+
+  breakpoint.addEventListener("change", responsive);
+  responsive(breakpoint);
+}
--- js/index_dom.js
+++ js/index_dom.js
@@ -6,6 +6,7 @@ import { moveBall, shorcuts } from "./dom/teclado.js";
 import countdown from "./dom/cuenta_regresiva.js";
 import scrollTopButton from "./dom/boton_scroll.js";
 import darkTheme from "./dom/tema_oscuro.js";
+import responsiveMedia from "./dom/objeto_responsive.js";
 
 const d = document;
 
@@ -15,6 +16,8 @@ d.addEventListener("DOMContentLoaded", (e) => {
   alarm("assets/alarma.mp3", "#activar-alarma", "#desactivar-alarma");
   countdown("countdown", "Oct 05, 2022 09:56:19", "Feliz cumpleaños, amigo y docente digital 🤓");
   scrollTopButton(".scroll-top-btn");
+  responsiveMedia("youtube", "(min-width: 1024px", `<a href="https://www.youtube.com/watch?v=2SetvwBV-SU&list=PLvq-jIkSeTUZ6QgYYO3MwG9EMqC-KoLXA&index=2" target="_blank" rel="noopener">Ver Video</a>`, `<iframe width="560" height="315" src="https://www.youtube.com/embed/2SetvwBV-SU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`);
+  responsiveMedia("gmaps", "(min-width: 1024px", `<a href="https://goo.gl/maps/cWam4uKMT1ijekcw8" target="_blank" rel="noopener">Ver Mapa</a>`, `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1881.3307860567459!2d-99.16869637401255!3d19.42702312620684!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1ff35f5bd1563%3A0x6c366f0e2de02ff7!2sEl%20%C3%81ngel%20de%20la%20Independencia!5e0!3m2!1ses!2ses!4v1665063898599!5m2!1ses!2ses" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`);
 });
 
 d.addEventListener("keydown", (e) => {
