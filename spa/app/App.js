23f2b0ebb0b2baa1fd4eb05273f27a4c7eca250f
Juan <juan@Juans-Mac-Pro.local>
2023-01-29 19:40:20 +0100

    Update 33

--- spa/app/App.js
+++ spa/app/App.js
@@ -1,18 +1,29 @@
 import api from "./helpers/wp_api.js";
 import {ajax} from "./helpers/ajax.js";
-import {Title} from "./components/Title.js"
 import {Loader} from "./components/Loader.js"
+import { Header } from "./components/Header.js";
+import { Posts } from "./components/Posts.js";
+import { PostCard } from "./components/PostCard.js";
 
 export function App () {
   const d = document,
     $root = d.getElementById("root");
 
-  $root.appendChild(Title());
+  $root.appendChild(Header());
+  $root.appendChild(Posts());
   $root.appendChild(Loader());
+
+  //console.log(api);
 
   ajax({
     url: api.POSTS,
-    cbSuccess: () => {}
-  })
+    cbSuccess: (posts) => {
+      console.log(posts);
+      let html = "";
+      posts.forEach(post => html += PostCard(post));
+      d.querySelector(".loader").style.display = "none";
+      d.getElementById("posts").innerHTML = html;
+    }
+  });
 }
 
