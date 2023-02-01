6e9a9a4162823c0a5b83b4df31966331da0a3512
Juan <juan@Juans-Mac-Pro.local>
2023-02-01 20:11:14 +0100

    Update 35

--- spa/app/components/Router.js
+++ spa/app/components/Router.js
@@ -4,33 +4,32 @@ import wp_api from "../helpers/wp_api.js";
 import { PostCard } from "./PostCard.js";
 
 
-export function Router() {
+export async function Router() {
   const d = document,
   w = window,
-  $posts = d.getElementById("posts");
+  $main = d.getElementById("main");
 
   let {hash} = location;
 
-  $posts.innerHTML = null;
+  $main.innerHTML = null;
 
   if(!hash || hash === "#/") {
-    ajax({
+    await ajax({
       url: wp_api.POSTS,
-      cbSuccess: (posts) => {
-        //console.log(posts);
+      cbSuccess: (main) => {
+        //console.log(main);
         let html = "";
-        posts.forEach(post => (html += PostCard(post)));
-        d.querySelector(".loader").style.display = "none";
-        $posts.innerHTML = html;
+        main.forEach(post => (html += PostCard(post)));
+        $main.innerHTML = html;
       }
     });
   } else if (hash.includes ("#/search")){
-    $posts.innerHTML = "<h2>Sección del Buscador</h2>"
+    $main.innerHTML = "<h2>Sección del Buscador</h2>";
   } else if (hash === "#/contacto"){
-    $posts.innerHTML = "<h2>Sección de Contacto</h2>"
+    $main.innerHTML = "<h2>Sección de Contacto</h2>";
   } else {
-    $posts.innerHTML = "<h2>Aquí cargará el contenido del post previamente seleccionado</h2>"
+    $main.innerHTML = "<h2>Aquí cargará el contenido del post previamente seleccionado</h2>";
   }
 
-
+  d.querySelector(".loader").style.display = "none";
 }
