ad2b28a45f989580be7f45c5d2f66fa6f54f843d
Juan <juan@Juans-Mac-Pro.local>
2023-02-02 19:36:53 +0100

    Update 36

--- spa/app/components/Router.js
+++ spa/app/components/Router.js
@@ -1,7 +1,8 @@
 import { ajax } from "../helpers/ajax.js"
 import wp_api from "../helpers/wp_api.js";
-
+import { Post } from "./Post.js";
 import { PostCard } from "./PostCard.js";
+import { SearchCard } from "./SearchCard.js";
 
 
 export async function Router() {
@@ -23,12 +24,46 @@ export async function Router() {
         $main.innerHTML = html;
       }
     });
+    console.log(wp_api.POST);
   } else if (hash.includes ("#/search")){
-    $main.innerHTML = "<h2>Sección del Buscador</h2>";
+    //$main.innerHTML = "<h2>Sección del Buscador</h2>";
+    let query= localStorage.getItem("wpSearch");
+
+    if(!query) {
+      d.querySelector(".loader").style.display = "none";
+      return false
+    };
+
+    await ajax({
+      url:`${wp_api.SEARCH}${query}`,
+      cbSuccess: (search) => {
+        //console.log(search);
+        let html = "";
+        if(search.length === 0){
+          html = `
+            <p class="error">
+            No existen resultados de búsqueda para el término
+            <mark>${query}</mark>.
+            </p>
+          `;
+        } else {
+          search.forEach(post => html += (SearchCard(post)));
+        }
+
+        $main.innerHTML = html;
+      }
+    });
+
   } else if (hash === "#/contacto"){
     $main.innerHTML = "<h2>Sección de Contacto</h2>";
   } else {
-    $main.innerHTML = "<h2>Aquí cargará el contenido del post previamente seleccionado</h2>";
+    await ajax({
+      url: `${wp_api.POST}/${localStorage.getItem("wpPostId")}`,
+      cbSuccess: (post) => {
+       //console.log(post);
+        $main.innerHTML = Post(post);
+      }
+    });
   }
 
   d.querySelector(".loader").style.display = "none";
